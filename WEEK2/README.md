86f3e6276a6d95c496614170b257428faa9b1a3a
VictorBoYang <VictorBoYang@gmail.com>
2019-09-11 23:11:05 -0400

    first version finish, only bubble sortingfor now

--- WEEK2/README.md
+++ WEEK2/README.md
@@ -1,1 +1,15 @@
 # Card_Sorter
+Card Sorter
+   Using C++, Java, or Python, design a program that creates an array of Card objects, shuffles them, then sorts them by value and then sorts them by value and suit.
+You will need to create a Card class with the following two fields:
+• value (a String)
+• suit (a String)
+You may include any other fields, constructors, or methods as needed.
+Your single array will need to contain 52 card objects, one for each value of each suit. The values to use are 2-10, J, Q, K, and A, with A being the card with the highest value. The suit values to use are H (for Hearts), D (for Diamonds), C (for Clubs), and S (for Spades).
+After building the array, shuffle the array using the Fisher-Yates Algorithm. You must implement this algorithm yourself.
+After shuffling, print each object’s value and suit to show the array was shuffled. The output for each object printed should be VALUE - SUIT. For example: 2 - H.
+Then, you’ll need to sort the array based on the values. All 2 cards should be at the beginning and all A cards should be at the end; The suit is irrelevant here. After sorting, print each object’s value and suit again to show the array was sorted based on the cards’ values.
+• If completing this project in Java, you may use the Comparable interface and/or the Comparator interface and the Arrays.sort() method. If completing this project in C++ or Python, you must choose and implement a sorting algorithm yourself. You may choose any sorting algorithm shown in the Module 1 lecture slides. You can also implement a sorting algorithm yourself if you are using Java.
+Finally, you’ll sort the array based on the values and suit. Clubs should be first, then Diamonds, then Hearts, then Spades. Each suit should be sorted from 2-A. After sorting, print each object’s value and suit again to show the array was sorted based on the cards’ values and suits.
+• If completing this project in Java, you may again use the Comparable interface and/or the Comparator class and the Arrays.sort() method. If completing this project in C++ or Python, you must choose and implement a sorting algorithm yourself. You may choose any sorting algorithm
+shown in the Module 1 lecture slides. You can also implement a sorting algorithm yourself if you are using Java. You can use the same algorithm for both sorts, if you wish.
---
+++ WEEK2/function_storage.py
@@ -0,0 +1,76 @@
+import random
+
+#Fisher-Yates Algorithm
+def shuffle(ary):
+    a=len(ary)
+    b=a-1
+    for d in range(b,0,-1):
+      e=random.randint(0,d)
+      if e == d:
+            continue
+      ary[d],ary[e]=ary[e],ary[d]
+    return ary
+
+#The function can print all 52 Card object in card array
+def print_CardArray(array):
+    for i in range(len(array)):
+        print(array[i].value,"-",array[i].suit)
+
+# The function for creating Card array
+def create_CardArray(value_array,suit_array,card_array):
+    for i,value in enumerate(value_array):
+        for j,suit in enumerate(suit_array):
+                card_array[i*4+j] = Card(value,suit)
+
+def convert_to_int(array):
+    for i in range(len(array)):
+        if array[i].value == "J":
+            array[i].value = "11"
+        elif array[i].value == "Q":
+            array[i].value = "12"
+        elif array[i].value == "K":
+            array[i].value = "13"
+        elif array[i].value == "A":
+            array[i].value = "14"
+        if array[i].suit == "C":
+            array[i].suit = "1"
+        elif array[i].suit == "D":
+            array[i].suit = "2"
+        elif array[i].suit == "H":
+            array[i].suit = "3"
+        elif array[i].suit == "S":
+            array[i].suit = "4"
+
+def convert_to_str(array):
+    for i in range(len(array)):
+        if array[i].value == "11":
+            array[i].value = "J"
+        elif array[i].value == "12":
+            array[i].value = "Q"
+        elif array[i].value == "13":
+            array[i].value = "K"
+        elif array[i].value == "14":
+            array[i].value = "A"
+        if array[i].suit == "1":
+            array[i].suit = "C"
+        elif array[i].suit == "2":
+            array[i].suit = "D"
+        elif array[i].suit == "3":
+            array[i].suit = "H"
+        elif array[i].suit == "4":
+            array[i].suit = "S"
+
+
+#Bubble sorting
+def bubble_sorting(array):
+    for i in range(len(array)):
+        for j in range(len(array)-1):
+            if int(array[j].value) > int(array[j+1].value):#This if will sort by Card.value
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            if int(array[j].value) == int(array[j+1].value):#This if will sorted by Card.suit
+                if int(array[j].suit) > int(array[j+1].suit):
+                    temp = array[j]
+                    array[j] = array[j+1]
+                    array[j+1] = temp
