7a9d05781ef678d8d1ec5dbed51e96a73560b18a
VictorBoYang <VictorBoYang@gmail.com>
2019-09-18 13:37:55 -0400

    remove bubble sort from function_storage

--- WEEK2/function_storage.py
+++ WEEK2/function_storage.py
@@ -24,17 +24,3 @@ def convert_to_int(array):
 def convert_to_str(array):
     for i in range(len(array)):
         array[i].int_to_str()
-
-#Bubble sorting
-def bubble_sorting(array):
-    for i in range(len(array)):
-        for j in range(len(array)-1):
-            if array[j].value > array[j+1].value:#This if will sort by Card.value
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
-            if array[j].value == array[j+1].value:#This if will sorted by Card.suit
-                if array[j].suit > array[j+1].suit:
-                    temp = array[j]
-                    array[j] = array[j+1]
-                    array[j+1] = temp
