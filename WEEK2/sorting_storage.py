2d4f9f06a31d75a2b28fa7a9b4d99cf074f04b81
VictorBoYang <VictorBoYang@gmail.com>
2019-09-21 11:41:23 -0400

    Correction algorithm

--- WEEK2/sorting_storage.py
+++ WEEK2/sorting_storage.py
@@ -4,12 +4,12 @@ from Card_class import *
 def bubble_sorting(array):
     for i in range(len(array)):
         for j in range(len(array)-1):
-            if array[j].value > array[j+1].value:#This if will sort by Card.value
+            if array[j].suit > array[j+1].suit:#This if will sort by Card.value
                 temp = array[j]
                 array[j] = array[j+1]
                 array[j+1] = temp
-            if array[j].value == array[j+1].value:#This if will sorted by Card.suit
-                if array[j].suit > array[j+1].suit:
+            if array[j].suit == array[j+1].suit:#This if will sorted by Card.suit
+                if array[j].value > array[j+1].value:
                     temp = array[j]
                     array[j] = array[j+1]
                     array[j+1] = temp
@@ -19,12 +19,12 @@ def recursive_bubble_sorting(array,len):
     if len == 0:
         return
     for j in range(len - 1):
-        if array[j].value > array[j+1].value:#This if will sort by Card.value
+        if array[j].suit > array[j+1].suit:#This if will sort by Card.value
             temp = array[j]
             array[j] = array[j+1]
             array[j+1] = temp
-        if array[j].value == array[j+1].value:#This if will sorted by Card.suit
-            if array[j].suit > array[j+1].suit:
+        if array[j].suit == array[j+1].suit:#This if will sorted by Card.suit
+            if array[j].value > array[j+1].value:
                 temp = array[j]
                 array[j] = array[j+1]
                 array[j+1] = temp
